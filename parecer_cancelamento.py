986421be4e3cb8fba5a10daf44e5aa0790d19371
Ricardo Job <ricardojob@gmail.com>
2014-12-23 18:10:11 -0300

    adição do parecer

--- parecer_cancelamento.py
+++ parecer_cancelamento.py
@@ -1,23 +1,19 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+from hermes import Hermes
 from datetime import datetime
-from subprocess import call
+from hermes import months
 
-months = ['', 'Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
-model      = open('tex/parecer_cancelamento.tex', 'r').read().replace('@@@', '%s')
-now        = datetime.now()
 student    = raw_input('Aluno: ')
-matricula = raw_input('Matrícula: ')
 processo = raw_input('Processo: ')
-disciplina     = raw_input('Disciplina: ')
-today      = '%s de %s de %s' % (now.day, months[now.month], now.year)
-result     = model % (today, processo, student, matricula, disciplina)
-file_name  = 'parecer - cancelamento de matricula em disciplina - %d.%02d.%02d - %s' % (now.year, now.month, now.day, student)
-tex = open(file_name + '.tex', 'w')
-tex.write(result)
-tex.close()
-call(['pdflatex', file_name + '.tex'])
-call(['rm',       file_name + '.tex'])
-call(['rm',       file_name + '.aux'])
-call(['rm',       file_name + '.log'])
-call(['mv',       file_name + '.pdf', str(now.year) + '/' + file_name + '.pdf'])
+enrollment = raw_input('Matricula: ')
+disciplina = raw_input('Disciplina: ')
+now        = datetime.now()
+hoje       = '%s de %s de %s' % (now.day, months[now.month], now.year)
+
+
+template = Hermes('parecer_cancelamento')
+data = {'@ALUNO' : student, '@DISCIPLINA' : disciplina, '@MATRICULA' : enrollment, \
+        '@HOJE' : hoje, '@PROCESSO': processo}
+
+template.build(data, student)
