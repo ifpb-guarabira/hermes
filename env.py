94d220f60c206de79302d3b59e17582a8ad16830
Moisés Guimarães <moises.medeiros@ifpb.edu.br>
2015-05-07 15:52:35 -0300

    corrigindo memo.

--- env.py
+++ env.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 context = {
-    '@ESTADO'  : 'Paraíba',
-    '@CIDADE'  : 'Cajazeiras',
-    '@USUÁRIO' : 'Ricardo de Sousa Job',
-    '@CURSO'   : 'Curso Técnico Integrado em Informática'
+    '@INSTITUTO' : 'IFPB',
+    '@ESTADO'    : 'Paraíba',
+    '@CIDADE'    : 'Cajazeiras',
+    '@USUÁRIO'   : 'Ricardo de Sousa Job',
+    '@CURSO'     : 'Curso Técnico Integrado em Informática',
+    '@SETOR'     : 'CTIN'
 }
--- memo.py
+++ memo.py
@@ -1,39 +1,18 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from hermes import Hermes
-from hermes import months
-from datetime import datetime
-from subprocess import call
-import os #Importo o modulo "os"
-import sys
+import hermes
+import env
 
-#Lista os arquivos e gera a numeração automatica do MEMO
-diretorioDeMemo = '~/Dropbox/CTIN/Documentos/2015/MEMO'
-arquivos = os.listdir(os.path.expanduser(diretorioDeMemo)  )
-num = len(arquivos) # quantidade de arquivos na pasta 
+template = hermes.Template('memo')
 
-numeroFormatado = ''
-for align, text in zip('>', [str(num +1)]):
-	numeroFormatado+='{0:{fill}{align}3}'.format(text, fill=align, align=align)
-numeroFormatado = numeroFormatado.replace('>', '0')
+def cli():
+    context = dict(env.context)
+    context['@NUMERO']  = raw_input('Número (ex. 001/2015): ')
+    context['@DESTINO'] = raw_input('Setor de Destino: ')
+    context['@ASSUNTO'] = raw_input('Assunto: ')
+    context['@TEXTO']   = open(raw_input('Corpo (arquivo): ')).read()
 
+    hermes.cli_builder(template, context, context['@NUMERO'].replace('/', '_'))
 
-data = {}
-
-data['@NUMERO']   = str(numeroFormatado) + '/'+ '2015'
-data['@DESTINO']  = raw_input('Setor de Destino: ')
-data['@ASSUNTO']  = raw_input('Assunto: ')
-
-arq = open('memo.txt', 'r')
-texto = arq.read()
-arq.close()
-
-data['@TEXTO']  = texto
-
-now        = datetime.now()
-hoje       = '%s de %s de %s' % (now.day, months[now.month], now.year)
-
-data['@HOJE']  = hoje
-
-template = Hermes('memo')
-template.build(data, data['@NUMERO'])
+if __name__ == '__main__':
+    cli()
