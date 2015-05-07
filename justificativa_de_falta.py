6bfeaababc8511751da31d2b8c3bbbb0ffd380d4
Moisés Guimarães <moises.medeiros@ifpb.edu.br>
2015-05-07 15:22:42 -0300

    corrigindo justificativa de falta

--- justificativa_de_falta.py
+++ justificativa_de_falta.py
@@ -1,34 +1,28 @@
-#!/usr/local/bin/python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-from datetime import datetime
-from datetime import timedelta
-from hermes import Hermes
-from hermes import months
+from datetime import datetime, timedelta
+import hermes
+import env
 
-data = {}
+comunicado = hermes.Template('justificativa_de_falta_comunicado')
+parecer    = hermes.Template('justificativa_de_falta_parecer')
 
-<<<<<<< HEAD
-model      = open('tex/justificativa_de_falta.tex', 'r').read().replace('@@@', '%s')
-now        = datetime.now()
-today      = '%s de %s de %s' % (now.day, months[now.month].upper(), now.year)
-student    = raw_input('Nome do aluno: ')
-year       = raw_input('Turma: ')
-dd, mm, yy = raw_input('Data do atestado: ').split('/')
-days       = int(raw_input('Duração (dias): '))
-license    = '%s/%s/%s' % (dd, mm, yy)
-=======
-data['@ALUNO']   = raw_input('Aluno: ')
-data['@ANO']     = raw_input('Turma: ')
-dd, mm, yy       = raw_input('Data do atestado: ').split('/')
-days             = int(raw_input('Duração (dias): '))
-data['@PERIODO'] = '%s/%s/%s' % (dd, mm, yy)
->>>>>>> 20bc99db544afab2ef68ae452918f499e5056b4f
+def cli():
+    context = dict(env.context)
+    context['@ALUNO']     = raw_input('Aluno: ')
+    context['@ANO']       = raw_input('Turma: ')
+    dd, mm, yy            = raw_input('Data do atestado: ').split('/')
+    days                  = int(raw_input('Duração (dias): '))
+    context['@PERIODO']   = '%s/%s/%s' % (dd, mm, yy)
 
-if days > 1:
-    end  = datetime(int(yy), int(mm), int(dd))
-    end += timedelta(days=days - 1)
+    if days > 1:
+        end  = datetime(int(yy), int(mm), int(dd))
+        end += timedelta(days=days - 1)
 
-    data['@PERIODO'] += ' à %02d/%02d/%04d' % (end.day, end.month, end.year)
+        context['@PERIODO'] += ' à %02d/%02d/%04d' % (end.day, end.month, end.year)
 
-Hermes('justificativa_de_falta').build(data, data['@ALUNO'])
-Hermes('comunicado_justificativa_de_falta').build(data, data['@ALUNO'])
+    hermes.cli_builder(comunicado, context, context['@ALUNO'])
+    hermes.cli_builder(parecer, context, context['@ALUNO'])
+
+if __name__ == '__main__':
+    cli()
